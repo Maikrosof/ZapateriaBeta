4f3c0f2405eaa1e1eabf130f976d207f89114745
Leandro <gonzleanjavi@gmail.com>
2018-09-30 23:01:01 -0300

    Boton Agregar de Productos
    Funcional ( SE PUEDE MEJORAR )

---
+++ Zapateria/Alta_Producto.Designer.vb
@@ -0,0 +1,266 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Alta_Producto
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim ID_ProveedorLabel As System.Windows.Forms.Label
+        Dim TallaLabel As System.Windows.Forms.Label
+        Dim PrecioLabel As System.Windows.Forms.Label
+        Dim ColorLabel As System.Windows.Forms.Label
+        Dim EstiloLabel As System.Windows.Forms.Label
+        Dim StockLabel As System.Windows.Forms.Label
+        Dim ID_MarcaLabel As System.Windows.Forms.Label
+        Me.ZapateriaDataSet = New Zapateria.ZapateriaDataSet()
+        Me.ProductosBindingSource = New System.Windows.Forms.BindingSource(Me.components)
+        Me.ProductosTableAdapter = New Zapateria.ZapateriaDataSetTableAdapters.ProductosTableAdapter()
+        Me.TableAdapterManager = New Zapateria.ZapateriaDataSetTableAdapters.TableAdapterManager()
+        Me.MarcasBindingSource = New System.Windows.Forms.BindingSource(Me.components)
+        Me.MarcasTableAdapter = New Zapateria.ZapateriaDataSetTableAdapters.MarcasTableAdapter()
+        Me.ID_ProveedorTextBox = New System.Windows.Forms.TextBox()
+        Me.TallaTextBox = New System.Windows.Forms.TextBox()
+        Me.PrecioTextBox = New System.Windows.Forms.TextBox()
+        Me.ColorTextBox = New System.Windows.Forms.TextBox()
+        Me.EstiloTextBox = New System.Windows.Forms.TextBox()
+        Me.StockTextBox = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.ID_MarcaTextBox = New System.Windows.Forms.TextBox()
+        ID_ProveedorLabel = New System.Windows.Forms.Label()
+        TallaLabel = New System.Windows.Forms.Label()
+        PrecioLabel = New System.Windows.Forms.Label()
+        ColorLabel = New System.Windows.Forms.Label()
+        EstiloLabel = New System.Windows.Forms.Label()
+        StockLabel = New System.Windows.Forms.Label()
+        ID_MarcaLabel = New System.Windows.Forms.Label()
+        CType(Me.ZapateriaDataSet, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.ProductosBindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.MarcasBindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'ZapateriaDataSet
+        '
+        Me.ZapateriaDataSet.DataSetName = "ZapateriaDataSet"
+        Me.ZapateriaDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema
+        '
+        'ProductosBindingSource
+        '
+        Me.ProductosBindingSource.DataMember = "Productos"
+        Me.ProductosBindingSource.DataSource = Me.ZapateriaDataSet
+        '
+        'ProductosTableAdapter
+        '
+        Me.ProductosTableAdapter.ClearBeforeFill = True
+        '
+        'TableAdapterManager
+        '
+        Me.TableAdapterManager.BackupDataSetBeforeUpdate = False
+        Me.TableAdapterManager.ClientesTableAdapter = Nothing
+        Me.TableAdapterManager.EmpleadosTableAdapter = Nothing
+        Me.TableAdapterManager.LocalidadTableAdapter = Nothing
+        Me.TableAdapterManager.MarcasTableAdapter = Me.MarcasTableAdapter
+        Me.TableAdapterManager.ProductosTableAdapter = Me.ProductosTableAdapter
+        Me.TableAdapterManager.ProveedoresTableAdapter = Nothing
+        Me.TableAdapterManager.UpdateOrder = Zapateria.ZapateriaDataSetTableAdapters.TableAdapterManager.UpdateOrderOption.InsertUpdateDelete
+        Me.TableAdapterManager.VentasTableAdapter = Nothing
+        '
+        'MarcasBindingSource
+        '
+        Me.MarcasBindingSource.DataMember = "Marcas"
+        Me.MarcasBindingSource.DataSource = Me.ZapateriaDataSet
+        '
+        'MarcasTableAdapter
+        '
+        Me.MarcasTableAdapter.ClearBeforeFill = True
+        '
+        'ID_ProveedorLabel
+        '
+        ID_ProveedorLabel.AutoSize = True
+        ID_ProveedorLabel.Location = New System.Drawing.Point(60, 152)
+        ID_ProveedorLabel.Name = "ID_ProveedorLabel"
+        ID_ProveedorLabel.Size = New System.Drawing.Size(73, 13)
+        ID_ProveedorLabel.TabIndex = 5
+        ID_ProveedorLabel.Text = "ID Proveedor:"
+        '
+        'ID_ProveedorTextBox
+        '
+        Me.ID_ProveedorTextBox.DataBindings.Add(New System.Windows.Forms.Binding("Text", Me.ProductosBindingSource, "ID_Proveedor", True))
+        Me.ID_ProveedorTextBox.Location = New System.Drawing.Point(139, 149)
+        Me.ID_ProveedorTextBox.Name = "ID_ProveedorTextBox"
+        Me.ID_ProveedorTextBox.Size = New System.Drawing.Size(100, 20)
+        Me.ID_ProveedorTextBox.TabIndex = 6
+        '
+        'TallaLabel
+        '
+        TallaLabel.AutoSize = True
+        TallaLabel.Location = New System.Drawing.Point(60, 178)
+        TallaLabel.Name = "TallaLabel"
+        TallaLabel.Size = New System.Drawing.Size(33, 13)
+        TallaLabel.TabIndex = 7
+        TallaLabel.Text = "Talla:"
+        '
+        'TallaTextBox
+        '
+        Me.TallaTextBox.DataBindings.Add(New System.Windows.Forms.Binding("Text", Me.ProductosBindingSource, "Talla", True))
+        Me.TallaTextBox.Location = New System.Drawing.Point(139, 175)
+        Me.TallaTextBox.Name = "TallaTextBox"
+        Me.TallaTextBox.Size = New System.Drawing.Size(100, 20)
+        Me.TallaTextBox.TabIndex = 8
+        '
+        'PrecioLabel
+        '
+        PrecioLabel.AutoSize = True
+        PrecioLabel.Location = New System.Drawing.Point(60, 204)
+        PrecioLabel.Name = "PrecioLabel"
+        PrecioLabel.Size = New System.Drawing.Size(40, 13)
+        PrecioLabel.TabIndex = 9
+        PrecioLabel.Text = "Precio:"
+        '
+        'PrecioTextBox
+        '
+        Me.PrecioTextBox.DataBindings.Add(New System.Windows.Forms.Binding("Text", Me.ProductosBindingSource, "Precio", True))
+        Me.PrecioTextBox.Location = New System.Drawing.Point(139, 201)
+        Me.PrecioTextBox.Name = "PrecioTextBox"
+        Me.PrecioTextBox.Size = New System.Drawing.Size(100, 20)
+        Me.PrecioTextBox.TabIndex = 10
+        '
+        'ColorLabel
+        '
+        ColorLabel.AutoSize = True
+        ColorLabel.Location = New System.Drawing.Point(60, 230)
+        ColorLabel.Name = "ColorLabel"
+        ColorLabel.Size = New System.Drawing.Size(34, 13)
+        ColorLabel.TabIndex = 11
+        ColorLabel.Text = "Color:"
+        '
+        'ColorTextBox
+        '
+        Me.ColorTextBox.DataBindings.Add(New System.Windows.Forms.Binding("Text", Me.ProductosBindingSource, "Color", True))
+        Me.ColorTextBox.Location = New System.Drawing.Point(139, 227)
+        Me.ColorTextBox.Name = "ColorTextBox"
+        Me.ColorTextBox.Size = New System.Drawing.Size(100, 20)
+        Me.ColorTextBox.TabIndex = 12
+        '
+        'EstiloLabel
+        '
+        EstiloLabel.AutoSize = True
+        EstiloLabel.Location = New System.Drawing.Point(60, 256)
+        EstiloLabel.Name = "EstiloLabel"
+        EstiloLabel.Size = New System.Drawing.Size(35, 13)
+        EstiloLabel.TabIndex = 13
+        EstiloLabel.Text = "Estilo:"
+        '
+        'EstiloTextBox
+        '
+        Me.EstiloTextBox.DataBindings.Add(New System.Windows.Forms.Binding("Text", Me.ProductosBindingSource, "Estilo", True))
+        Me.EstiloTextBox.Location = New System.Drawing.Point(139, 253)
+        Me.EstiloTextBox.Name = "EstiloTextBox"
+        Me.EstiloTextBox.Size = New System.Drawing.Size(100, 20)
+        Me.EstiloTextBox.TabIndex = 14
+        '
+        'StockLabel
+        '
+        StockLabel.AutoSize = True
+        StockLabel.Location = New System.Drawing.Point(60, 282)
+        StockLabel.Name = "StockLabel"
+        StockLabel.Size = New System.Drawing.Size(38, 13)
+        StockLabel.TabIndex = 15
+        StockLabel.Text = "Stock:"
+        '
+        'StockTextBox
+        '
+        Me.StockTextBox.DataBindings.Add(New System.Windows.Forms.Binding("Text", Me.ProductosBindingSource, "Stock", True))
+        Me.StockTextBox.Location = New System.Drawing.Point(139, 279)
+        Me.StockTextBox.Name = "StockTextBox"
+        Me.StockTextBox.Size = New System.Drawing.Size(100, 20)
+        Me.StockTextBox.TabIndex = 16
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(338, 318)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(134, 95)
+        Me.Button1.TabIndex = 19
+        Me.Button1.Text = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'ID_MarcaLabel
+        '
+        ID_MarcaLabel.AutoSize = True
+        ID_MarcaLabel.Location = New System.Drawing.Point(60, 126)
+        ID_MarcaLabel.Name = "ID_MarcaLabel"
+        ID_MarcaLabel.Size = New System.Drawing.Size(54, 13)
+        ID_MarcaLabel.TabIndex = 19
+        ID_MarcaLabel.Text = "ID Marca:"
+        AddHandler ID_MarcaLabel.Click, AddressOf Me.ID_MarcaLabel_Click
+        '
+        'ID_MarcaTextBox
+        '
+        Me.ID_MarcaTextBox.DataBindings.Add(New System.Windows.Forms.Binding("Text", Me.ProductosBindingSource, "ID_Marca", True))
+        Me.ID_MarcaTextBox.Location = New System.Drawing.Point(139, 123)
+        Me.ID_MarcaTextBox.Name = "ID_MarcaTextBox"
+        Me.ID_MarcaTextBox.Size = New System.Drawing.Size(100, 20)
+        Me.ID_MarcaTextBox.TabIndex = 20
+        '
+        'Alta_Producto
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(541, 433)
+        Me.Controls.Add(ID_MarcaLabel)
+        Me.Controls.Add(Me.ID_MarcaTextBox)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(ID_ProveedorLabel)
+        Me.Controls.Add(Me.ID_ProveedorTextBox)
+        Me.Controls.Add(TallaLabel)
+        Me.Controls.Add(Me.TallaTextBox)
+        Me.Controls.Add(PrecioLabel)
+        Me.Controls.Add(Me.PrecioTextBox)
+        Me.Controls.Add(ColorLabel)
+        Me.Controls.Add(Me.ColorTextBox)
+        Me.Controls.Add(EstiloLabel)
+        Me.Controls.Add(Me.EstiloTextBox)
+        Me.Controls.Add(StockLabel)
+        Me.Controls.Add(Me.StockTextBox)
+        Me.Name = "Alta_Producto"
+        Me.Text = "Alta_Producto"
+        CType(Me.ZapateriaDataSet, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.ProductosBindingSource, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.MarcasBindingSource, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents ZapateriaDataSet As Zapateria.ZapateriaDataSet
+    Friend WithEvents ProductosBindingSource As System.Windows.Forms.BindingSource
+    Friend WithEvents ProductosTableAdapter As Zapateria.ZapateriaDataSetTableAdapters.ProductosTableAdapter
+    Friend WithEvents TableAdapterManager As Zapateria.ZapateriaDataSetTableAdapters.TableAdapterManager
+    Friend WithEvents MarcasTableAdapter As Zapateria.ZapateriaDataSetTableAdapters.MarcasTableAdapter
+    Friend WithEvents MarcasBindingSource As System.Windows.Forms.BindingSource
+    Friend WithEvents ID_ProveedorTextBox As System.Windows.Forms.TextBox
+    Friend WithEvents TallaTextBox As System.Windows.Forms.TextBox
+    Friend WithEvents PrecioTextBox As System.Windows.Forms.TextBox
+    Friend WithEvents ColorTextBox As System.Windows.Forms.TextBox
+    Friend WithEvents EstiloTextBox As System.Windows.Forms.TextBox
+    Friend WithEvents StockTextBox As System.Windows.Forms.TextBox
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents ID_MarcaTextBox As System.Windows.Forms.TextBox
+End Class
---
+++ Zapateria/Principal_Producto.Designer.vb
@@ -0,0 +1,232 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Principal_Producto
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.ZapateriaDataSet = New Zapateria.ZapateriaDataSet()
+        Me.ProductosBindingSource = New System.Windows.Forms.BindingSource(Me.components)
+        Me.ProductosTableAdapter = New Zapateria.ZapateriaDataSetTableAdapters.ProductosTableAdapter()
+        Me.TableAdapterManager = New Zapateria.ZapateriaDataSetTableAdapters.TableAdapterManager()
+        Me.ProductosDataGridView = New System.Windows.Forms.DataGridView()
+        Me.DataGridViewTextBoxColumn1 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.DataGridViewTextBoxColumn2 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.DataGridViewTextBoxColumn3 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.DataGridViewTextBoxColumn4 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.DataGridViewTextBoxColumn5 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.DataGridViewTextBoxColumn6 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.DataGridViewTextBoxColumn7 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.DataGridViewTextBoxColumn8 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.BtnAgregar = New System.Windows.Forms.Button()
+        Me.MarcasBindingSource = New System.Windows.Forms.BindingSource(Me.components)
+        Me.MarcasTableAdapter = New Zapateria.ZapateriaDataSetTableAdapters.MarcasTableAdapter()
+        Me.BtnEliminar = New System.Windows.Forms.Button()
+        Me.BtnModificar = New System.Windows.Forms.Button()
+        Me.BtnBuscar = New System.Windows.Forms.Button()
+        Me.BtnSalir = New System.Windows.Forms.Button()
+        CType(Me.ZapateriaDataSet, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.ProductosBindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.ProductosDataGridView, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.MarcasBindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'ZapateriaDataSet
+        '
+        Me.ZapateriaDataSet.DataSetName = "ZapateriaDataSet"
+        Me.ZapateriaDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema
+        '
+        'ProductosBindingSource
+        '
+        Me.ProductosBindingSource.DataMember = "Productos"
+        Me.ProductosBindingSource.DataSource = Me.ZapateriaDataSet
+        '
+        'ProductosTableAdapter
+        '
+        Me.ProductosTableAdapter.ClearBeforeFill = True
+        '
+        'TableAdapterManager
+        '
+        Me.TableAdapterManager.BackupDataSetBeforeUpdate = False
+        Me.TableAdapterManager.ClientesTableAdapter = Nothing
+        Me.TableAdapterManager.EmpleadosTableAdapter = Nothing
+        Me.TableAdapterManager.LocalidadTableAdapter = Nothing
+        Me.TableAdapterManager.MarcasTableAdapter = Me.MarcasTableAdapter
+        Me.TableAdapterManager.ProductosTableAdapter = Me.ProductosTableAdapter
+        Me.TableAdapterManager.ProveedoresTableAdapter = Nothing
+        Me.TableAdapterManager.UpdateOrder = Zapateria.ZapateriaDataSetTableAdapters.TableAdapterManager.UpdateOrderOption.InsertUpdateDelete
+        Me.TableAdapterManager.VentasTableAdapter = Nothing
+        '
+        'ProductosDataGridView
+        '
+        Me.ProductosDataGridView.AutoGenerateColumns = False
+        Me.ProductosDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.ProductosDataGridView.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.DataGridViewTextBoxColumn1, Me.DataGridViewTextBoxColumn2, Me.DataGridViewTextBoxColumn3, Me.DataGridViewTextBoxColumn4, Me.DataGridViewTextBoxColumn5, Me.DataGridViewTextBoxColumn6, Me.DataGridViewTextBoxColumn7, Me.DataGridViewTextBoxColumn8})
+        Me.ProductosDataGridView.DataSource = Me.ProductosBindingSource
+        Me.ProductosDataGridView.Location = New System.Drawing.Point(0, 31)
+        Me.ProductosDataGridView.Name = "ProductosDataGridView"
+        Me.ProductosDataGridView.Size = New System.Drawing.Size(737, 220)
+        Me.ProductosDataGridView.TabIndex = 1
+        '
+        'DataGridViewTextBoxColumn1
+        '
+        Me.DataGridViewTextBoxColumn1.DataPropertyName = "ID_Zap"
+        Me.DataGridViewTextBoxColumn1.HeaderText = "ID_Zap"
+        Me.DataGridViewTextBoxColumn1.Name = "DataGridViewTextBoxColumn1"
+        Me.DataGridViewTextBoxColumn1.ReadOnly = True
+        '
+        'DataGridViewTextBoxColumn2
+        '
+        Me.DataGridViewTextBoxColumn2.DataPropertyName = "ID_Marca"
+        Me.DataGridViewTextBoxColumn2.HeaderText = "ID_Marca"
+        Me.DataGridViewTextBoxColumn2.Name = "DataGridViewTextBoxColumn2"
+        '
+        'DataGridViewTextBoxColumn3
+        '
+        Me.DataGridViewTextBoxColumn3.DataPropertyName = "ID_Proveedor"
+        Me.DataGridViewTextBoxColumn3.HeaderText = "ID_Proveedor"
+        Me.DataGridViewTextBoxColumn3.Name = "DataGridViewTextBoxColumn3"
+        '
+        'DataGridViewTextBoxColumn4
+        '
+        Me.DataGridViewTextBoxColumn4.DataPropertyName = "Talla"
+        Me.DataGridViewTextBoxColumn4.HeaderText = "Talla"
+        Me.DataGridViewTextBoxColumn4.Name = "DataGridViewTextBoxColumn4"
+        '
+        'DataGridViewTextBoxColumn5
+        '
+        Me.DataGridViewTextBoxColumn5.DataPropertyName = "Precio"
+        Me.DataGridViewTextBoxColumn5.HeaderText = "Precio"
+        Me.DataGridViewTextBoxColumn5.Name = "DataGridViewTextBoxColumn5"
+        '
+        'DataGridViewTextBoxColumn6
+        '
+        Me.DataGridViewTextBoxColumn6.DataPropertyName = "Color"
+        Me.DataGridViewTextBoxColumn6.HeaderText = "Color"
+        Me.DataGridViewTextBoxColumn6.Name = "DataGridViewTextBoxColumn6"
+        '
+        'DataGridViewTextBoxColumn7
+        '
+        Me.DataGridViewTextBoxColumn7.DataPropertyName = "Estilo"
+        Me.DataGridViewTextBoxColumn7.HeaderText = "Estilo"
+        Me.DataGridViewTextBoxColumn7.Name = "DataGridViewTextBoxColumn7"
+        '
+        'DataGridViewTextBoxColumn8
+        '
+        Me.DataGridViewTextBoxColumn8.DataPropertyName = "Stock"
+        Me.DataGridViewTextBoxColumn8.HeaderText = "Stock"
+        Me.DataGridViewTextBoxColumn8.Name = "DataGridViewTextBoxColumn8"
+        '
+        'BtnAgregar
+        '
+        Me.BtnAgregar.Location = New System.Drawing.Point(0, 275)
+        Me.BtnAgregar.Name = "BtnAgregar"
+        Me.BtnAgregar.Size = New System.Drawing.Size(146, 67)
+        Me.BtnAgregar.TabIndex = 2
+        Me.BtnAgregar.Text = "Agregar"
+        Me.BtnAgregar.UseVisualStyleBackColor = True
+        '
+        'MarcasBindingSource
+        '
+        Me.MarcasBindingSource.DataMember = "Marcas"
+        Me.MarcasBindingSource.DataSource = Me.ZapateriaDataSet
+        '
+        'MarcasTableAdapter
+        '
+        Me.MarcasTableAdapter.ClearBeforeFill = True
+        '
+        'BtnEliminar
+        '
+        Me.BtnEliminar.Location = New System.Drawing.Point(152, 275)
+        Me.BtnEliminar.Name = "BtnEliminar"
+        Me.BtnEliminar.Size = New System.Drawing.Size(146, 67)
+        Me.BtnEliminar.TabIndex = 3
+        Me.BtnEliminar.Text = "Eliminar"
+        Me.BtnEliminar.UseVisualStyleBackColor = True
+        '
+        'BtnModificar
+        '
+        Me.BtnModificar.Location = New System.Drawing.Point(304, 275)
+        Me.BtnModificar.Name = "BtnModificar"
+        Me.BtnModificar.Size = New System.Drawing.Size(146, 67)
+        Me.BtnModificar.TabIndex = 4
+        Me.BtnModificar.Text = "Modificar"
+        Me.BtnModificar.UseVisualStyleBackColor = True
+        '
+        'BtnBuscar
+        '
+        Me.BtnBuscar.Location = New System.Drawing.Point(456, 275)
+        Me.BtnBuscar.Name = "BtnBuscar"
+        Me.BtnBuscar.Size = New System.Drawing.Size(146, 67)
+        Me.BtnBuscar.TabIndex = 5
+        Me.BtnBuscar.Text = "Buscar"
+        Me.BtnBuscar.UseVisualStyleBackColor = True
+        '
+        'BtnSalir
+        '
+        Me.BtnSalir.Location = New System.Drawing.Point(580, 372)
+        Me.BtnSalir.Name = "BtnSalir"
+        Me.BtnSalir.Size = New System.Drawing.Size(146, 67)
+        Me.BtnSalir.TabIndex = 6
+        Me.BtnSalir.Text = "Salir"
+        Me.BtnSalir.UseVisualStyleBackColor = True
+        '
+        'Principal_Producto
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(738, 451)
+        Me.Controls.Add(Me.BtnSalir)
+        Me.Controls.Add(Me.BtnBuscar)
+        Me.Controls.Add(Me.BtnModificar)
+        Me.Controls.Add(Me.BtnEliminar)
+        Me.Controls.Add(Me.BtnAgregar)
+        Me.Controls.Add(Me.ProductosDataGridView)
+        Me.Name = "Principal_Producto"
+        Me.Text = "Principal_Producto"
+        CType(Me.ZapateriaDataSet, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.ProductosBindingSource, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.ProductosDataGridView, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.MarcasBindingSource, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents ZapateriaDataSet As Zapateria.ZapateriaDataSet
+    Friend WithEvents ProductosBindingSource As System.Windows.Forms.BindingSource
+    Friend WithEvents ProductosTableAdapter As Zapateria.ZapateriaDataSetTableAdapters.ProductosTableAdapter
+    Friend WithEvents TableAdapterManager As Zapateria.ZapateriaDataSetTableAdapters.TableAdapterManager
+    Friend WithEvents MarcasTableAdapter As Zapateria.ZapateriaDataSetTableAdapters.MarcasTableAdapter
+    Friend WithEvents ProductosDataGridView As System.Windows.Forms.DataGridView
+    Friend WithEvents DataGridViewTextBoxColumn1 As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents DataGridViewTextBoxColumn2 As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents DataGridViewTextBoxColumn3 As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents DataGridViewTextBoxColumn4 As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents DataGridViewTextBoxColumn5 As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents DataGridViewTextBoxColumn6 As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents DataGridViewTextBoxColumn7 As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents DataGridViewTextBoxColumn8 As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents BtnAgregar As System.Windows.Forms.Button
+    Friend WithEvents MarcasBindingSource As System.Windows.Forms.BindingSource
+    Friend WithEvents BtnEliminar As System.Windows.Forms.Button
+    Friend WithEvents BtnModificar As System.Windows.Forms.Button
+    Friend WithEvents BtnBuscar As System.Windows.Forms.Button
+    Friend WithEvents BtnSalir As System.Windows.Forms.Button
+End Class
